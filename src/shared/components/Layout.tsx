3bad98f81f2f019390bb182736f1800bc463764b
João Vitor <jvitordiass@outlook.com>
2022-03-25 16:49:01 -0300

    implements layout component

---
+++ src/shared/components/Layout.tsx
@@ -0,0 +1,46 @@
+import { Container, IconButton, TextField } from "@mui/material"
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { ReactNode } from "react"
+import { Link } from "react-router-dom"
+import { Header } from "./Header"
+
+type Props = {
+  title: string
+  children: ReactNode
+}
+
+export const Layout: React.FC<Props> = ({ title, children }) => {
+  return (
+    <>
+      <Header title={title} />
+      <Container sx={{ 
+        backgroundColor: 'background.default', 
+        textAlign: 'right'
+      }}>
+        <TextField 
+          label="Digite sua busca..." 
+          variant="outlined" 
+          sx={{
+            width: "100%",
+            marginTop: 2
+          }}
+        />
+        {children}
+        <Link to="product/create" style={{
+          position: 'sticky',
+          bottom: '16px',
+        }}>
+          <IconButton sx={{ 
+            padding: 0,
+            color: 'primary.main'
+          }}>
+            <AddCircleIcon sx={{
+              fontSize: '3rem',
+            }} />
+          </IconButton>
+        </Link>
+      </Container>
+    </>
+  )
+}
+  

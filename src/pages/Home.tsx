90e86b7cf9f57fb40b74ac1c26faefa92a787a17
João Vitor <jvitordiass@outlook.com>
2022-03-24 23:31:27 -0300

    implements home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,57 @@
-import { Container, TextField } from "@mui/material"
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { Container, IconButton, TextField } from "@mui/material"
+import AddCircleIcon from '@mui/icons-material/AddCircle'
 import { Card, Header }from "../shared/components"
 
-import { products } from "../shared/repositories/products"
+import { products as p } from "../shared/repositories/products"
 
 export const Home: React.FC = () => {
   const [ companies, setCompanies ] = useState<string[]>([])
+  const [ products, setProducts ] = useState<string[]>([])
 
   useEffect(() => {
     const companies: string[] = []
-    products.forEach(({ availability }) => {
+    p.forEach(({ availability }) => {
       availability.forEach(({ company }) => {
         companies.push(company)
       })
     })
+    const products = p.map(({ name }) => name)
     setCompanies(companies)
+    setProducts(products)
   }, [])
+
   return (
     <>
     <Header title="Pedidos" />  
-    <Container>
-    <TextField 
-      label="Digite sua busca..." 
-      variant="outlined" 
-      sx={{
-        width: "100%",
-        marginY: 2
-      }}
-    />
+    <Container sx={{ 
+      backgroundColor: 'background.default', 
+      textAlign: 'right'
+    }}>
+      <TextField 
+        label="Digite sua busca..." 
+        variant="outlined" 
+        sx={{
+          width: "100%",
+          marginTop: 2
+        }}
+      />
       <Card title="Empresas" items={companies} />
+      <Card title="Produtos" items={products} />
+      <Link to="product/create" style={{
+        position: 'sticky',
+        bottom: '16px',
+      }}>
+        <IconButton sx={{ 
+          padding: 0,
+          color: 'primary.main'
+        }}>
+          <AddCircleIcon sx={{
+            fontSize: '3rem',
+          }} />
+        </IconButton>
+      </Link>
     </Container>
     </>
   )

66b90b70244eaaa3cdd49dd1d984cbde4e25cedb
João Vitor <jvitordiass@outlook.com>
2022-03-24 21:48:20 -0300

    adds data example

---
+++ src/shared/repositories/products.ts
@@ -0,0 +1,41 @@
+export const products = [
+  {
+    name: 'Água Sanitária',
+    description: '1L', 
+    availability: [{
+      brand: 'Teiú',
+      price: 2.15,
+      company: 'Teiú',
+    }, {
+      brand: 'Zab',
+      price: 1.85,
+      company: 'Zab'
+    }] 
+  },
+  {
+    name: 'Água Sanitária',
+    description: '1L', 
+    availability: [{
+      brand: 'Teiú',
+      price: 2.15,
+      company: 'Teiú',
+    }, {
+      brand: 'Zab',
+      price: 1.85,
+      company: 'Zab'
+    }] 
+  },
+  {
+    name: 'Água Sanitária',
+    description: '1L', 
+    availability: [{
+      brand: 'Teiú',
+      price: 2.15,
+      company: 'Teiú',
+    }, {
+      brand: 'Zab',
+      price: 1.85,
+      company: 'Zab'
+    }] 
+  }
+]

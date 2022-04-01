d05e326ed2ab02a4ae3f31e56ee58be3ecd1f9b2
João Vitor <jvitordiass@outlook.com>
2022-03-31 21:01:34 -0300

    fix: renames env variables and update the import

--- src/shared/environment/firebase-config.ts
+++ src/shared/environment/firebase-config.ts
@@ -1,14 +1,16 @@
 import * as firebase from "firebase/app"
+import * as auth from "firebase/auth"
 import * as firestore from 'firebase/firestore'
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_API_KEY as string,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_APP_ID as string,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID as string
 })
 
+export const authConfig = auth.getAuth(app)
 export const db = firestore.getFirestore(app)

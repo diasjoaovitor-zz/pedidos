f5274f0ee5c08603f2c0c2f42ed41a83272dbe9b
João Vitor <jvitordiass@outlook.com>
2022-03-31 21:04:47 -0300

    feat: implements loader

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,37 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { Layout, Card }from "../shared/components"
+import { Layout, Card, Loader }from "../shared/components"
 import { products as p } from "../shared/repositories/products"
 
 export const Home: React.FC = () => {
   const navigate = useNavigate()
   const [ companies, setCompanies ] = useState<string[]>([])
   const [ products, setProducts ] = useState<string[]>([])
+  const [ loader, setLoader ] = useState(true)
 
   useEffect(() => {
-    const companies: string[] = []
-    p.forEach(({ availability }) => {
-      availability.forEach(({ company }) => {
-        companies.push(company)
+    try {
+      const companies: string[] = []
+      p.forEach(({ availability }) => {
+        availability.forEach(({ company }) => {
+          companies.push(company)
+        })
       })
-    })
-    const products = p.map(({ name }) => name)
-    setCompanies(companies)
-    setProducts(products)
+      const products = p.map(({ name }) => name)
+      setCompanies(companies)
+      setProducts(products)
+    } catch (error) {
+      alert('ops')
+    } finally {
+      setLoader(false)
+    }
   }, [])
 
   return (
     <Layout title="Pedidos" handleFocus={() => navigate('/search')}>
       <Card title="Empresas" items={companies} />
       <Card title="Produtos" items={products} />
+      {loader && <Loader />}
     </Layout>
   )
 }

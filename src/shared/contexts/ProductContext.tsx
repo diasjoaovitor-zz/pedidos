4359aa82b4d7ead754078a58fb65544f881562ff
João Vitor <jvitordiass@outlook.com>
2022-04-07 17:09:21 -0300

    feat: adds other contexts

--- src/shared/contexts/ProductContext.tsx
+++ src/shared/contexts/ProductContext.tsx
@@ -3,9 +3,13 @@ import { productState } from "../states"
 import { TProduct, TProductPresentation } from "../types"
 
 type TProductContext = {
+  updateData: boolean
   productContext: TProduct
+  productsContext: TProduct[]
   productPresentationContext: TProductPresentation
+  setUpdateData(condition: boolean): void
   setProductContext(product: TProduct): void
+  setProductsContext(products: TProduct[]): void
   setProductPresentationContext(product: TProductPresentation): void
 }
 
@@ -14,14 +18,18 @@ const ProductContext = createContext({} as TProductContext)
 export const useProductContext = () => useContext(ProductContext)
 
 export const ProductProvider: React.FC = ({ children }) => {
+  const [ updateData, setUpdateData ] = useState(true)
   const [ product, setProduct ] = useState(productState)
+  const [ products, setProducts ] = useState([productState])
   const [ productPresentation, setProductPresentation ] = useState({} as TProductPresentation)
 
   return (
     <ProductContext.Provider 
       value={{ 
+        updateData, setUpdateData,
         productContext: product, setProductContext: setProduct, 
-        productPresentationContext: productPresentation, setProductPresentationContext: setProductPresentation 
+        productPresentationContext: productPresentation, setProductPresentationContext: setProductPresentation,
+        productsContext: products, setProductsContext: setProducts, 
       }}
     >
      {children}

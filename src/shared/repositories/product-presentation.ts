95d586c09f1d22bb70784f3de78be01f33d1e600
João Vitor <jvitordiass@outlook.com>
2022-04-10 18:50:24 -0300

    chore: removes repositories

--- src/shared/repositories/product-presentation.ts
+++
@@ -1,9 +0,0 @@
-import { TProductPresentation } from "../types";
-
-export const productPresentation: TProductPresentation = {
-  name: "Água Sanitária", 
-  description: "1L", 
-  brand: "Zab",
-  company: "Zab",
-  price: 1.85
-}

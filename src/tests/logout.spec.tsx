8ad0aa08b02dc2cbacae453c48a236c25333bce1
João Vitor <jvitordiass@outlook.com>
2022-05-16 17:22:14 -0300

    test: logout

---
+++ src/tests/logout.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen, waitFor } from "@testing-library/react"
+import userEvent from '@testing-library/user-event'
+import { createMemoryHistory } from "history"
+import { Route } from "./utils/Route"
+import { Home } from "../pages"
+import { TLogoutService } from "../shared/types"
+import { AppThemeProvider, ProductProvider } from "../shared/contexts"
+
+jest.mock('firebase/auth')
+
+const history = createMemoryHistory({ initialEntries: ['/'] })
+
+const setup = (logout: TLogoutService) => {
+  render(
+    <Route history={history}>
+      <AppThemeProvider>
+        <ProductProvider>
+          <Home logout={logout} />
+        </ProductProvider>
+      </AppThemeProvider>
+    </Route>
+  )
+  return screen
+}
+
+describe('<Home />', () => {
+  it('Logout failed', async () => {
+    const logout = jest.fn()
+    logout.mockImplementation(() => {
+      throw { code: 'abc' }
+    })
+    const screen = setup(logout)
+    const menu = screen.getByRole('navigation')
+    await userEvent.click(menu)
+    const logoutButton = screen.getByText('Logout')
+    await userEvent.click(logoutButton)
+
+    await waitFor(() => expect(screen.queryByText('Algo deu errado!')).toBeInTheDocument())
+  })
+})

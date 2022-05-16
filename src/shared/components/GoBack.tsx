8eb0dab6d1a5d9978f494283ff70484b094e749f
João Vitor <jvitordiass@outlook.com>
2022-05-16 17:16:31 -0300

    refactor: simplifies go back navigation

--- src/shared/components/GoBack.tsx
+++ src/shared/components/GoBack.tsx
@@ -1,11 +1,15 @@
 import { ArrowBack } from "@mui/icons-material"
 import { IconButton } from "@mui/material"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
-export const GoBack: React.FC<{ to?: string }> = ({ to }) => (
-  <Link to={to || '/'}>
-    <IconButton sx={{ color: 'background.default' }} >
-      <ArrowBack />
-    </IconButton>
-  </Link>
-)
+export const GoBack: React.FC = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Link to="/" onClick={() => navigate(-1)}>
+      <IconButton sx={{ color: 'background.default' }} >
+        <ArrowBack />
+      </IconButton>
+    </Link>
+  )
+}

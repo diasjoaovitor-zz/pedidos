6c3d0c1cc6217f5fcf4ed37b6425f3a8556ac8f4
João Vitor <jvitordiass@outlook.com>
2022-05-23 23:20:09 -0300

    refactor: creates components from the product page

---
+++ src/shared/components/ProductForm.tsx
@@ -0,0 +1,55 @@
+import { FormEvent, ReactNode } from "react";
+import { Box, Button, Card, Container, TextField } from "@mui/material";
+import { handleFocus } from "../functions";
+
+type Props = {
+  name: string
+  description: string
+  buttonTitle: string
+  children: ReactNode
+  handleSubmit(e: FormEvent<HTMLElement>): Promise<void>
+}
+export const ProductForm: React.FC<Props> = ({ name, description, buttonTitle, children, handleSubmit }) => (
+  <Card 
+    component="form"
+    sx={{
+      paddingX: 2,
+      paddingY: 4,
+      '& .MuiTextField-root': {
+        marginBottom: 4,
+      },
+      '& .availability': {
+        display: 'flex',
+        overflowX: 'auto'
+      }
+    }}
+    onSubmit={handleSubmit}
+    >
+      <Container>
+        <Box component="section">
+          <TextField 
+            name="name"
+            label="Produto" 
+            variant="outlined" 
+            fullWidth
+            required
+            defaultValue={name}
+            onFocus={handleFocus}
+          />
+          <TextField 
+            name="description"
+            label="Descrição" 
+            variant="outlined" 
+            fullWidth
+            defaultValue={description}
+          />
+        </Box>
+        <div className="availability">
+          {children}
+        </div>
+        <Button type="submit" variant="contained" fullWidth sx={{ marginTop: 4 }}>
+          {buttonTitle}
+        </Button>
+      </Container>
+    </Card>
+)

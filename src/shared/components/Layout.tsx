ce10b2b8324d27432e1210129995edfaf8b7b47c
João Vitor <jvitordiass@outlook.com>
2022-05-23 23:12:39 -0300

    refactor: removes handle key and header in layout

--- src/shared/components/Layout.tsx
+++ src/shared/components/Layout.tsx
@@ -1,68 +1,62 @@
 import { useNavigate } from "react-router-dom"
 import { Container, IconButton, TextField } from "@mui/material"
 import { AddCircle } from '@mui/icons-material';
-import { ChangeEvent, KeyboardEvent, ReactNode } from "react"
+import { ChangeEvent, ReactNode } from "react"
 import { Link } from "react-router-dom"
 import { useProductContext } from "../contexts"
 import { TProductPresentation } from "../types"
 import { productState } from "../states";
 
 type Props = {
-  header: ReactNode
   children: ReactNode
   autoFocus?: boolean
   handleChange?: (e: ChangeEvent<HTMLInputElement>) => void
-  handleKey?: (e: KeyboardEvent<HTMLInputElement>) => void
 }
 
-export const Layout: React.FC<Props> = ({ header, children, autoFocus, handleChange, handleKey }) => {
+export const Layout: React.FC<Props> = ({ children, autoFocus, handleChange }) => {
   const navigate = useNavigate()
   const { setProductContext, setProductPresentationContext } = useProductContext()
 
   return (
-    <>
-      {header}
-      <Container sx={{ 
-        backgroundColor: 'background.default', 
-        textAlign: 'right'
-      }}>
-        <TextField 
-          label="Digite sua busca..." 
-          variant="outlined" 
-          type="search"
-          sx={{
-            width: "100%",
-            marginTop: 2
-          }}
-          autoFocus={autoFocus}
-          onFocus={() => !autoFocus && navigate('/search')}
-          onChange={handleChange}
-          onKeyDown={handleKey}
-        />
-        {children}
-        <Link 
-          to="/product/create" 
-          style={{
-            position: 'sticky',
-            bottom: '16px',
-          }}
-          onClick={() => {
-            setProductContext(productState)
-            setProductPresentationContext({} as TProductPresentation)
-          }}
-          role="insertion"
-        >
-          <IconButton sx={{ 
-            padding: 0,
-            color: 'primary.main'
-          }}>
-            <AddCircle sx={{
-              fontSize: '3rem',
-            }} />
-          </IconButton>
-        </Link>
-      </Container>
-    </>
+    <Container sx={{ 
+      backgroundColor: 'background.default', 
+      textAlign: 'right'
+    }}>
+      <TextField 
+        label="Digite sua busca..." 
+        variant="outlined" 
+        type="search"
+        sx={{
+          width: "100%",
+          marginTop: 2
+        }}
+        autoFocus={autoFocus}
+        onFocus={() => !autoFocus && navigate('/search')}
+        onChange={handleChange}
+      />
+      {children}
+      <Link 
+        to="/product/create" 
+        style={{
+          position: 'sticky',
+          bottom: '16px',
+        }}
+        onClick={() => {
+          setProductContext(productState)
+          setProductPresentationContext({} as TProductPresentation)
+        }}
+        role="insertion"
+      >
+        <IconButton sx={{ 
+          padding: 0,
+          color: 'primary.main'
+        }}>
+          <AddCircle sx={{
+            fontSize: '3rem',
+          }} />
+        </IconButton>
+      </Link>
+    </Container>
   )
 }
   

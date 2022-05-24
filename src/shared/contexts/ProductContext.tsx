8974b7696f883504d343d4276b6d0083058dc7c3
João Vitor <jvitordiass@outlook.com>
2022-05-23 23:24:54 -0300

    refactor: removes unused items because of graphql

--- src/shared/contexts/ProductContext.tsx
+++ src/shared/contexts/ProductContext.tsx
@@ -2,13 +2,9 @@ import { createContext, ReactNode, useContext, useState } from "react"
 import { TProduct, TProductPresentation } from "../types"
 
 type TProductContext = {
-  updateData: boolean
   productContext: TProduct
-  productsContext: TProduct[]
   productPresentationContext: TProductPresentation
-  setUpdateData(condition: boolean): void
   setProductContext(product: TProduct): void
-  setProductsContext(products: TProduct[]): void
   setProductPresentationContext(product: TProductPresentation): void
 }
 
@@ -17,18 +13,14 @@ const ProductContext = createContext({} as TProductContext)
 export const useProductContext = () => useContext(ProductContext)
 
 export const ProductProvider: React.FC<{children: ReactNode, product: TProduct}> = ({ children, product: productState }) => {
-  const [ updateData, setUpdateData ] = useState(true)
   const [ product, setProduct ] = useState(productState)
-  const [ products, setProducts ] = useState([productState])
   const [ productPresentation, setProductPresentation ] = useState({} as TProductPresentation)
 
   return (
     <ProductContext.Provider 
       value={{ 
-        updateData, setUpdateData,
         productContext: product, setProductContext: setProduct, 
         productPresentationContext: productPresentation, setProductPresentationContext: setProductPresentation,
-        productsContext: products, setProductsContext: setProducts, 
       }}
     >
      {children}

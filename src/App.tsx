4cc53d2d8a9a605df13084a7e0de5cff022e23c8
João Vitor <jvitordiass@outlook.com>
2022-05-23 23:01:30 -0300

    feat: adds apollo graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { ApolloProvider } from '@apollo/client'
 import { Router } from './Router'
 import { ProductProvider, AppThemeProvider, AuthProvider } from './shared/contexts'
+import { client } from './shared/environment'
 import { productState } from './shared/states'
 
 const App: React.FC = () => (
   <AppThemeProvider>
     <AuthProvider>
-      <ProductProvider product={productState}>
-        <Router />
-      </ProductProvider>
+      <ApolloProvider client={client}>
+        <ProductProvider product={productState}>
+          <Router />
+        </ProductProvider>
+      </ApolloProvider>
     </AuthProvider>
   </AppThemeProvider>
 )
